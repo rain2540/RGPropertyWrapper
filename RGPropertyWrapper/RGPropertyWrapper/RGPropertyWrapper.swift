1e9ea5bc0445cddeff689b877c9fa1411059be35
Rain2540 <rain2540@foxmail.com>
2020-04-23 23:57:45 +0800

    chore (project) : add RGPropertyWrapper

---
+++ RGPropertyWrapper/RGPropertyWrapper/RGPropertyWrapper.swift
@@ -0,0 +1,16 @@
+//
+//  RGPropertyWrapper.swift
+//  RGPropertyWrapper
+//
+//  Created by Rain on 2020/4/23.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import Foundation
+
+
+struct RGPropertyWrapper {
+    
+    
+    
+}
