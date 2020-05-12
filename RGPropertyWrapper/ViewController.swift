4324aec02dd4f12f818e5607339b84b6c6b8f3a9
Smartech <rain2540@foxmail.com>
2020-05-12 14:19:37 +0800

    feat (ViewController) : test

--- RGPropertyWrapper/ViewController.swift
+++ RGPropertyWrapper/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let testModel = TestModel()
+        
+        print(testModel.storeValue)
+        print(testModel.trim)
+        print(testModel.replace)
     }
 
 
