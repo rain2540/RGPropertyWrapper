3fb123dfbeaea0cbbead45a4936176173e039a65
Smartech <rain2540@foxmail.com>
2020-05-12 13:48:30 +0800

    chore (project) : add TestModel

---
+++ RGPropertyWrapper/TestModel.swift
@@ -0,0 +1,16 @@
+//
+//  TestModel.swift
+//  RGPropertyWrapper
+//
+//  Created by RAIN on 2020/5/12.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import Foundation
+
+
+struct TestModel {
+
+
+
+}
